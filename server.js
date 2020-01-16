60d124bd40a7e46f80899a363645ae67897fb1c5
specter25 <ujjwalcoding012@gmail.com>
2020-01-16 18:11:35 +0530

    deployment1

--- server.js
+++ server.js
@@ -15,14 +15,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-//server static assets in production
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static('client/build'))
+  // Set static folder
+  app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  });
 }
+
 
 
 const PORT = process.env.PORT || 5000;
